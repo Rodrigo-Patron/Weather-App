baea27f1f202d20751cd3d17059470dda5fef381
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-02-02 23:30:10 +0100

    frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+
+const App = () => {
+  const cityInputValue = useRef();
+
+  const [weather, setWeather] = useState();
+
+  const submitHandler = () => {
+    const city = { city: cityInputValue.current.value };
+
+    const config = {
+      method: "POST",
+      body: JSON.stringify(city),
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    fetch("http://localhost:8002/weather", config)
+      .then((response) => response.json())
+      .then((result) => setWeather(result));
+  };
+
+  return (
+    <div>
+      <input type="text" ref={cityInputValue} placeholder="Search City" />
+      <button onClick={submitHandler}>Submit</button>
+
+      {weather && (
+        <h3>
+          The temperature of {weather.city} is {weather.temperature}°C{" "}
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default App;
